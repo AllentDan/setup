4567b850baad7a1ab069040124df7d72ecf2bf12
AllentDan <AllentDan@yeah.net>
2021-12-03 11:09:35 +0800

    add vim config

--- zzvim.sh
+++ zzvim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-apt get install nvim
-
+apt-get install curl
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp init_vim ~/.vimrc
