692e2d48b84e8fda19c854789ffcf71cecc62a31
AllentDan <AllentDan@yeah.net>
2021-12-19 22:23:46 +0800

    refine git config script

--- zzgit_config.sh
+++ zzgit_config.sh
@@ -4,5 +4,8 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
 
-git config --global user.name "AllentDan"
-git config --global user.email "AllentDan@yeah.net"
+read -p "Input user name: " NAME
+read -p "Input user email: " EMAIL
+git config --global user.name $NAME 
+git config --global user.email $EMAIL
+git config -l
